bc4c8f7b8d1cb8aa92d69a1a2b7641c18315b647
Bea <bea_sm86@hotmail.com>
2019-01-20 19:30:35 +0000

    commit inicial

--- app/src/main/java/com/practica/bea_s/felicitacionnavidad/MainActivity.java
+++ app/src/main/java/com/practica/bea_s/felicitacionnavidad/MainActivity.java
@@ -1,8 +1,10 @@
 package com.practica.bea_s.felicitacionnavidad;
 
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Crear un objeto de tipo Typeface. Todas las fuentes que se quieran
+        Typeface tp = Typeface.createFromAsset(getAssets(),"ChristmasSparkle_PERSONAL.ttf" );
+        TextView tv_titulo = (TextView) findViewById(R.id.titulo);
+        tv_titulo.setTypeface(tp);
+
         //Crear un objeto de tipo MediaPlayer
-        MediaPlayer MP = MediaPlayer.create(this,R.raw.last_christmas);
-        MP.start();
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.last_christmas);
+        mp.start();
     }
 }
