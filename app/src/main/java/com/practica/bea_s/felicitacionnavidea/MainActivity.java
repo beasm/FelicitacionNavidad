70569b0b306608b0737c006e3e0355ec9557dc11
Bea <bea_sm86@hotmail.com>
2019-01-20 15:47:21 +0000

    Commit Inicial

---
+++ app/src/main/java/com/practica/bea_s/felicitacionnavidea/MainActivity.java
@@ -0,0 +1,19 @@
+package com.practica.bea_s.felicitacionnavidea;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Crear un objeto de tipo MediaPlayer
+        MediaPlayer MP = MediaPlayer.create(this,R.raw.last_christmas);
+        MP.start();
+    }
+}
